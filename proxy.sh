1cec75a547c870a5c09a4d3fc8d9f196315e27da
mrzhu666 <1052712811@qq.com>
2021-12-20 11:12:51 +0000

    update vue

--- proxy.sh
+++ proxy.sh
@@ -4,11 +4,14 @@
 # 服务器代理127.0.0.1
 # 虚拟机代理为主机ip
 # 混合代理设为socks5
-ip="192.168.10.133"
+server="192.168.10.133"
 port="7890"
 
 
 # git代理
-git config --global http.proxy "socks5://$ip:$port" && git config --global https.proxy "socks5://$ip:$port"
+git config --global http.proxy "socks5://$server:$port" && git config --global https.proxy "socks5://$server:$port"
 # proxychains
-
+# sudo vim /etc/proxychains.conf
+# npm代理
+npm config set proxy "http://$server:$port"
+npm config set https-proxy "http://$server:$port"
