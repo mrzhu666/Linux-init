8114ac1447770d3af2c266c83d990c4987321117
mrzhu666 <1052712811@qq.com>
2021-12-20 13:04:12 +0000

    update deeplearning

---
+++ deeplearning/package.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+init()
+{
+    echo "Anaconda环境管理"
+    echo "1.导出"
+    echo "2.导出"
+    read -r selecttion
+    echo "输入环境名称："
+    read -r env
+    case $selecttion in
+        1) 
+        exportEnv;;
+        2) 
+        importEnv;;
+        *) echo default
+        ;;
+    esac
+}
+
+
+exportEnv(){
+    source activate  # 激活基础环境，脚本文件是隔离运行的
+    conda activate "$env"
+    conda env export > "./out/$env".yaml
+    pip freeze > "./out/$env".txt
+}
+
+
+importEnv(){
+    conda env create -f "./out/$env".yaml -n "$env"
+    conda activate "$env" & :: source activate "$env"
+    pip install -r "./out/$env".txt
+}
+
+init
